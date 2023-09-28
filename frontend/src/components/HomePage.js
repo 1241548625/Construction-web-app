6e545242638068bfa0803e16566352095a4f3803
1241548625 <112222264+1241548625@users.noreply.github.com>
2023-09-28 18:22:16 -0400

    update

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "./Home.css";
-import HomeImage1 from "../asset/HomePage.jpg";
 
 const HomePage = () => {
   return (
@@ -10,19 +9,28 @@ const HomePage = () => {
       <div className="container">
         <div className="up-level">
           <div className="intro1">
-            {/* <h1>WE ARE BUILDING YOUR DREAM HOUSE</h1>
+            <h1>WE ARE BUILDING YOUR DREAM HOUSE</h1>
             <p>
               At Qu Construction Inc, we're more than just builders; we're
               creators of lasting impressions. Our commitment to excellence and
               passion for construction drive us to turn your vision into
               reality.
-            </p> */}
+            </p>
           </div>
-          <div className="picture1">
-            <image className="pic1" src={HomeImage1}></image>
-          </div>
+
+          <div className="picture1"></div>
         </div>
-        <div className="down-level"></div>
+        <div className="down-level">
+          <div className="intro2">
+            <p>
+              With years of experience and a team of skilled professionals, we
+              have established ourselves as a trusted name in the construction
+              industry. Our dedication to quality, innovation, and customer
+              satisfaction is at the core of everything we do.
+            </p>
+          </div>
+          <div className="picture2"></div>
+        </div>
       </div>
     </div>
   );

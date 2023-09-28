7efd7b59ec61efe99dc629be5e2173609710dbed
1241548625 <112222264+1241548625@users.noreply.github.com>
2023-09-27 22:32:46 -0400

    up

--- server/routes/submit.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import Users from "../controller/users.js";
-
-const router = express.Router();
-
-// HTTP request
-router.post("/contact/submit", Users);
-
-export default router;

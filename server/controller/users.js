e9faed34118f4bd793d1750d951d351b723441c8
1241548625 <112222264+1241548625@users.noreply.github.com>
2023-09-27 22:21:03 -0400

    update

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,10 +1,8 @@
 import UserSchema from "../model/User.js";
-import { Mailchimp } from "@mailchimp/mailchimp_marketing";
-
-const mailchimp = new Mailchimp(process.env.EMAILCHIMP);
+import nodemail from "nodemailer";
 
 // when user submit the form, create new user
-export const Users = async (req, res) => {
+const Users = async (req, res) => {
   try {
     const { FirstName, LastName, email, phone, message } = req.body;
 
@@ -18,8 +16,40 @@ export const Users = async (req, res) => {
 
     const saveduser = await newUser.save();
 
+    //test account
+    const testAccount = await nodemail.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "your@gmail.com", // Replace with your email address
+        pass: "your-password", // Replace with your email password
+      },
+    });
+
+    const info = {
+      from: '"Fred Foo" <foo@example.com>', //sender
+      to: "bar@example.com", //receiver
+      subject: "Hello", //plain text body
+      text: "hello world",
+    };
+
+    transporter
+      .sendMail(info)
+      .then(() => {
+        return res.status(201).json({ msg: "You should receive a email" });
+      })
+      .catch((err) => {
+        return res.status(500).json({ err });
+      });
+
     res.status(201).json({ message: "User created successfully" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+// const Users = (req, res) => {
+//   res.status(201).json("Signup successfully");
+// };
+
+export default Users;
--- server/routes/submit.js
+++ server/routes/submit.js
@@ -1,9 +1,9 @@
-import { express } from "express";
-import { users } from "../controller/users.js";
+import express from "express";
+import Users from "../controller/users.js";
 
 const router = express.Router();
 
 // HTTP request
-router.post("/contact/submit", users);
+router.post("/contact/submit", Users);
 
 export default router;
